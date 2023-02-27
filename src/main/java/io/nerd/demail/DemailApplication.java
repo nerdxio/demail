4f68e9b4bbd9e52d0ea7c0048109a64ddcb48e00
Hassan Refaat <hassan.refaat.dev@gmail.com>
2023-02-27 20:03:24 +0200

    Initial commit

---
+++ src/main/java/io/nerd/demail/DemailApplication.java
@@ -0,0 +1,24 @@
+package io.nerd.demail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemailApplication.class, args);
+    }
+
+    @RequestMapping("/user")
+    public String user(@AuthenticationPrincipal OAuth2User principal) {
+        System.out.println(principal);
+        return principal.getAttribute("name");
+    }
+
+}
---
+++ src/test/java/io/nerd/demail/DemailApplicationTests.java
@@ -0,0 +1,13 @@
+package io.nerd.demail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
